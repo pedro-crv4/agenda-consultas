c19c2e400b7187afc7905fc54e46295b4d6f9747
Pedro Carvalho <equype1@gmail.com>
2023-11-09 00:13:28 -0300

    create user resource

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,10 +1,16 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class Doctor {
+export class User {
     @PrimaryGeneratedColumn()
     id: number;
 
     @Column()
     firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column({ default: true })
+    isActive: boolean;
 }
